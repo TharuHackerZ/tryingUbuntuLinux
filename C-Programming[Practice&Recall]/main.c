2938b0968d4a5593627d021f1f8bca8dcea51cfb
prcdotio <chaudharyposhan57@gmail.com>
2026-01-26 18:12:36 +0545

    commandLineArgumentTest is added and fibonacci series calculator also added.

--- C-Programming[Practice&Recall]/main.c
+++ C-Programming[Practice&Recall]/main.c
@@ -1,26 +1,17 @@
-// Trying recursion to calculate factoral
+//fibonacci series
 
 #include <stdio.h>
-int fact(int num);
 int main(void){
-    int n;
-    printf("Enter any number to get it's corresponding factoral:\t");
-    scanf("%d", &n);
-    if(n < 0){
-        printf("F*ck U, Don't try to be oversmart. It's undefined for negative itegers.\n");
-        return -1;
-    }else if(n > 12){
-        printf("F*c* U, Don't try to be oversmart. It's just 64 bit system and compiler is just built for 32 bit handling. Don't try to calculate larger number otherwise you will get inaccurate value.\n");
-        return 13;
-    }else{
-    int result = fact(n);
-    printf("The corresponding factoral is: %d\n", result);
+    int limit, swap1 = 0,  swap2 = 1, swap3 = swap1 + swap2;
+    printf("Enter how many first fibonacci series number do you want to get[0 included]:\t");
+    scanf("%d", &limit);
+    printf("\n%d\t%d\t", swap1, swap2);
+    for(int i = 0; i <= limit-3; ++i){
+        swap1 = swap2;
+        swap2 = swap3;
+        printf("%d\t", swap3);
+        swap3 = swap1 + swap2;
     }
-
+    printf("\n");
     return 0;
-}
-int fact(int num){
-    if(num == 1 || num == 0)
-    return 1;
-    return num * fact(num - 1);
 }
