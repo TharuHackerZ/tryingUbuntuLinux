42c9760158d861f5fb5127a36517caa8aa8f1dc8
prcdotio <chaudharyposhan57@gmail.com>
2026-02-04 19:14:39 +0545

    using malloc function to create dynamic array and get some input and give sum as output

--- C-Programming[Practice&Recall]/main.c
+++ C-Programming[Practice&Recall]/main.c
@@ -1,11 +1,24 @@
 /*
-print something without using semi colon 
+In this challenge we will use malloc function for dynamic array and get n number of input and give sum as output of those nubmers
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 int main(void){
-  // using switch
-  switch(printf("Hello, World!!\n")) {}
+  int n;
+  printf("Enter how many numbers do you want to give input:\t");
+  scanf("%d", &n);
+  int *arr = (int *)malloc(n * sizeof(int));
+  printf("Enter numbers:\n");
+  for (int i = 0; i < n; ++i){
+    scanf("%d", &arr[i]);
+  }
+  int sum = 0;
+  for (int i = 0; i < n; ++i){
+    sum += arr[i];
+  }
+  printf("\n The sum is:\t%d.\n", sum);
 
-    return 0;
+  free(arr);
+  return 0;
 }
